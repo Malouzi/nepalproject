f1769ad78dc161137d450cb8b0a5d6dcd5ff0d4e
Malouzi <marie.zirzlaff@dci-student.org>
2025-02-24 19:42:43 +0100

    Home Seite erstellt

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,20 @@
 
+import './Home.css';
+
 const Home = () => {
     return (
         <div>
-            <h1>Willkommen auf der Startseite</h1>
+            <h1>6 Schulen, 6 Wochen - Bildung verbidet Welten</h1>
+            <p>Ein interkulturelles Bildungsprojekt zwischen Nepal und Deutschland.</p>
+            <iframe 
+                width="800" 
+                height="500" 
+                src="https://www.youtube.com/embed/iAuCSqNlc_Q" 
+                title="YouTube video" 
+                frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowFullScreen>
+            </iframe>
         </div>
     );
 };
